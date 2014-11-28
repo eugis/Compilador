1d3021f96401747cc5e8cf5168c9b2cc72ed521a
mariaeugenia <me.sakuda@hotmail.com>
2014-11-28 16:45:34 -0300

    Ejemplos pedidos. Hay que probarlos

--- src/Compilador.java
+++ src/Compilador.java
@@ -2,7 +2,7 @@ import java.io.FileReader;
 
 public class Compilador {
     public static void main(String[] args) throws Exception {
-        Parser parser_obj = new Parser(new Lexer(new FileReader(args[1])));
+        Parser parser_obj = new Parser(new Lexer(new FileReader(args[0])));
         Program parse_tree;
         try {
             parse_tree = (Program) parser_obj.parse().value;
